49558d47cab6e214784df41104502eacaa138ab0
Nikita Sergeev <nikitasergeev1357@gmail.com>
2024-07-04 11:00:26 +0300

    Add pagination

--- blog/models.py
+++ blog/models.py
@@ -16,7 +16,8 @@ class Post(models.Model):
         PUBLISHED = 'PB', 'Published'
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250, unique=True)
+    # Предотвратим хранение в модели Post дублирующихся записей, определив, что слаги являются уникальными для даты публикации поста
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
@@ -36,4 +37,9 @@ class Post(models.Model):
         return self.title
 
     def get_absolute_url(self):
-        return reverse('blog:post_detail', args=[self.id])
+        """Канонический URL адрес для модели"""
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])
